07e1a514617ce0bd4e6afda6aede9d793600078d
sozu00 <inhiguez.jesus@gmail.com>
2017-03-01 19:48:59 +0100

    ejercicio 3

--- EDNL/practica1.cpp
+++ EDNL/practica1.cpp
@@ -12,13 +12,27 @@ int numnodos(Abin<T>& a){
 }
 
 template <typename T>
-int NodoMasnumhijos(Abin<T>& A, typename Abin<T<::nodo n){
+int NodoMasnumhijos(Abin<T>& A, typename Abin<T>::nodo n){
   return (n == Abin::NODO_NULO) ? 0 : 1 + NodoMasnumhijos(A,A.hijoIzqdoB(n)) + NodoMasnumhijos(A, A.hijoDrchoB(n));
 }
 
 template <typename T>
 int altura(Abin<T>& A){
-  return (A.raizB() == Abin::NODO_NULO) ? 0 : 1 + max()
+  return alturaNodo(A, a.raizB());
+}
+
+template <typename T>
+int alturaNodo(Abin<T>& A, typename Abin<T>::nodo n){
+  return (n == NODO_NULO) ? -1 : 1 + max(alturaNodo(A, A.hijoIzqdoB(n)), alturaNodo(A, A.hijoDrchoB(n)));
+}
+
+template <typename T>
+int profundidadNodo(Abin<T>& A, typename Abin<T>::nodo n){
+  int prof = 0;
+  for(typename Abin<T>::nodo aux = n; A.padreB(aux) != Abin::NODO_NULO;  aux = A.padreB(aux)){
+    ++prof;
+  }
+  return prof;
 }
 
 
