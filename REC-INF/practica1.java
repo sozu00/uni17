d19bb40d24322fd48ee68135b0fa3dd2db0d901d
sozu00 <inhiguez.jesus@gmail.com>
2017-03-02 20:00:46 +0100

    ejercicios

---
+++ REC-INF/practica1.java
@@ -0,0 +1,74 @@
+import java.util.regex.*;
+public class practica1{
+  public static boolean uno(String cadena){
+    Pattern pat = Pattern.compile("abc.*");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean dos(String cadena){
+    Pattern pat = Pattern.compile("abc.*|Abc.*");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean tres(String cadena){
+    Pattern pat = Pattern.compile("\\D.*");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean cuatro(String cadena){
+    Pattern pat = Pattern.compile(".*\\D");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean cinco(String cadena){
+    Pattern pat = Pattern.compile("[al]");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean seis(String cadena){
+    Pattern pat = Pattern.compile(".*2[^6].*");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean siete(String cadena){
+    Pattern pat = Pattern.compile("[a-zA-z]{5,10}");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean ocho(String cadena){
+    Pattern pat = Pattern.compile("www[\\x2E].*[\\x2E]es");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean diez(String cadena){
+    Pattern pat = Pattern.compile("[0-9]{1,2}[\\x2F][0-9]{1,2}[\\x2F][0-9]{1,2}");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean diez(String cadena){
+    Pattern pat = Pattern.compile("[0-9]{1,3}[\\x2E][0-9]{1,3}[\\x2E][0-9]{1,3}[\\x2E][0-9]{1,3}");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean diez(String cadena){
+    Pattern pat = Pattern.compile("[0-9]{1,3}[\\x2E][0-9]{1,3}[\\x2E][0-9]{1,3}[\\x2E][0-9]{1,3}");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+
+  public static boolean once(String cadena){
+    Pattern pat = Pattern.compile("+34[ ][0-9]{2}[ ][0-9]{7}");
+    Matcher mat = pat.matcher(cadena);
+    return mat.matches();
+  }
+}
---
+++ REC-INF/pruebas.java
@@ -0,0 +1,9 @@
+public class pruebas {
+  public static void main(String[] args){
+
+    String cadena = "12/5/12";
+
+    if (practica1.nueve(cadena)) System.out.println("VERDADERO");
+    else System.out.println("FALSO");
+  }
+}
