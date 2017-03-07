ed88e3682a721fdcdab3f42600be3370193ce5f6
sozu00 <inhiguez.jesus@gmail.com>
2017-03-07 12:14:28 +0100

    Ordenes de recorrido en profundidad

--- EDNL/recorridos.cpp
+++ EDNL/recorridos.cpp
@@ -9,11 +9,29 @@ void recorridoPreorden(Abin<T> &A){
 }
 
 template <typename T>
-void recorridoNodoP(Abin<T> &A, typename Abin<T>::nodo n){
+void Preorden(Abin<T> &A, typename Abin<T>::nodo n){
   if(n != Abin<T>::NODO_NULO){
     procesar(A, n);
-    recorridoNodoP(A, A.hijoIzqdoB(A, n));
-    recorridoNodoP(A, A.hijoDrchoB(A, n));
+    Preorden(A, A.hijoIzqdoB(A, n));
+    Preorden(A, A.hijoDrchoB(A, n));
+  }
+}
+
+template <typename T>
+void Inorden(Abin<T> &A, typename Abin<T>::nodo n){
+  if(n != Abin<T>::NODO_NULO){
+    Inorden(A, A.hijoIzqdoB(A, n));
+    procesar(A, n);
+    Inorden(A, A.hijoDrchoB(A, n));
+  }
+}
+
+template <typename T>
+void Postorden(Abin<T> &A, typename Abin<T>::nodo n){
+  if(n != Abin<T>::NODO_NULO){
+    Postorden(A, A.hijoIzqdoB(A, n));
+    Postorden(A, A.hijoDrchoB(A, n));
+    procesar(A, n);
   }
 }
 
