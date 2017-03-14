ebe0bfc92fd5445f8f96cb236e666f73e6b912f2
sozu00 <inhiguez.jesus@gmail.com>
2017-03-14 17:35:25 +0100

    practica2

--- EDNL/practica1.cpp
+++ EDNL/practica1.cpp
@@ -1,8 +1,8 @@
 #include "abin.h"
+#include "abin_E-S.h"
 #include <iostream>
 
-template <typename T>
-int max(int a, b){
+int max(int a, int b){
   return (a > b)? a : b;
 }
 
@@ -11,39 +11,62 @@ int numnodos(Abin<T>& a){
   return NodoMasnumhijos(a, a.raizB());
 }
 
-
-
 template <typename T>
 int NodoMasnumhijos(Abin<T>& A, typename Abin<T>::nodo n){
-  return (n == Abin::NODO_NULO) ? 0 : 1 + NodoMasnumhijos(A,A.hijoIzqdoB(n)) + NodoMasnumhijos(A, A.hijoDrchoB(n));
+  return (n == Abin<T>::NODO_NULO) ? 0 : 1 + NodoMasnumhijos(A,A.hijoIzqdoB(n)) + NodoMasnumhijos(A, A.hijoDrchoB(n));
 }
 
 template <typename T>
 int altura(Abin<T>& A){
-  return alturaNodo(A, a.raizB());
+  return alturaNodo(A, A.raizB());
 }
 
 template <typename T>
 int alturaNodo(Abin<T>& A, typename Abin<T>::nodo n){
-  return (n == NODO_NULO) ? -1 : 1 + max(alturaNodo(A, A.hijoIzqdoB(n)), alturaNodo(A, A.hijoDrchoB(n)));
+  return (n == Abin<T>::NODO_NULO) ? -1 : 1 + max(alturaNodo(A, A.hijoIzqdoB(n)), alturaNodo(A, A.hijoDrchoB(n)));
 }
 
 template <typename T>
 int profundidadNodo(Abin<T>& A, typename Abin<T>::nodo n){
   int prof = 0;
-  for(typename Abin<T>::nodo aux = n; A.padreB(aux) != Abin::NODO_NULO;  aux = A.padreB(aux)){
-    ++prof;
-  }
+  for(typename Abin<T>::nodo aux = n; A.padreB(aux) != Abin<T>::NODO_NULO;  aux = A.padreB(aux)) ++prof;
   return prof;
+}
+
+template <typename T>
+int desequilibrioNodo(Abin<T>& A, typename Abin<T>::nodo n){
+  //Maxima diferencia de alturas
+  int difAlt = abs(alturaNodo(A, A.hijoIzqdoB(n) - alturaNodo(A, A.hijoDrchoB(n))));
+  int difMax = max(difAlt, max(desequilibrio(A, A.hijoIzqdoB(n)), desequilibrio(A, A.hijoDrchoB(n))));
+  return difMax;
+}
+
+template <typename T>
+int desequilibrio(Abin<T>& A){
+  return desequilibrioNodo(A, A.raizB());
+}
+
+template <typename T>
+bool pseudoCompleto(Abin<T>& A){
+  //Penultimo nivel lleno o vacío
+  //Busco nodos con profundidad = alturaArbol - 1
+  int penLvL = altura(A) - 1;
+  return cumplePenultimo(A, A.raizB(), penLvL);
+}
+
+template <typename T>
+bool cumplePenultimo(Abin<T>& A, typename Abin<T>::nodo n, int penLvL){
+  if(n == Abin<T>::NODO_NULO) return true;
+  if(profundidadNodo(A, n) == penLvL){
+    if(A.hijoIzqdoB(n) == Abin<T>::NODO_NULO) return (A.hijoDrchoB(n) == Abin<T>::NODO_NULO);
+    else return (A.hijoDrchoB(n) != Abin<T>::NODO_NULO);
+  }
+  else return (cumplePenultimo(A, A.hijoIzqdoB(n), penLvL) && cumplePenultimo(A, A.hijoDrchoB(n), penLvL));
 }
 
 
 int main(){
   Abin<int> a;
-  a.insertarRaizB(0);
-
-  a.insertarHijoIzqdoB(a.raizB(), 1);
-  a.insertarHijoDrchoB(a.raizB(), 2);
-  a.insertarHijoIzqdoB(a.hijoIzqdoB(a.raizB()), 3);
-  std::cout << numnodos(a);
+  rellenarAbin(a, 1);
+  std::cout << pseudoCompleto(a) << std::endl;
 }
---
+++ EDNL/practica2.cpp
@@ -0,0 +1,78 @@
+#include "abin.h"
+#include "abin_E-S.h"
+#include <iostream>
+
+template <typename T>
+bool similares(Abin<T>& A, Abin<T>& B){
+  subarbolSimilar(A,B,A.raizB(),B.raizB());
+}
+
+template <typename T>
+bool subarbolSimilar(Abin<T>& A, Abin<T>& B, typename Abin<T>::nodo nA, typename Abin<T>::nodo nB){
+  if(nA == Abin<T>::NODO_NULO)
+    return (nB == Abin<T>::NODO_NULO) ? true : false; //Si los 2 son nulos true, si A si y B no, false
+
+  if(nB == Abin<T>::NODO_NULO) //A NO es nulo ya
+    return false;
+
+  //Ni A ni B son nulos
+  return subarbolSimilar(A, B, A.hijoIzqdoB(nA), B.hijoIzqdoB(nB)) && subarbolSimilar(A, B, A.hijoDrchoB(nA), B.hijoDrchoB(nB));
+}
+
+
+template <typename T>
+Abin<T> reflejado(Abin<T>& A){
+  Abin<T> B;
+  if(A.raizB() != Abin<T>::NODO_NULO){
+    B.insertarRaizB(A.elemento(A.raizB()));
+    reflexion(A, B, A.raizB(), B.raizB());
+  }
+  return B;
+}
+
+template <typename T>
+void reflexion(Abin<T>& A,Abin<T>& B, typename Abin<T>::nodo nA, typename Abin<T>::nodo nB){
+  if(A.hijoDrchoB(nA) != Abin<T>::NODO_NULO){
+    B.insertarHijoIzqdoB(nB, A.elemento(A.hijoDrchoB(nA)));
+    reflexion(A,B,A.hijoDrchoB(nA), B.hijoIzqdoB(nB));
+  }
+
+  if(A.hijoIzqdoB(nA) != Abin<T>::NODO_NULO){
+    B.insertarHijoDrchoB(nB, A.elemento(A.hijoIzqdoB(nA)));
+    reflexion(A,B,A.hijoIzqdoB(nA), B.hijoDrchoB(nB));
+  }
+}
+
+struct componente{
+  double operando;
+  char operador;
+};
+
+double aritmeticaRec(Abin<componente>& A, typename Abin<componente>::nodo n){
+  if(A.hijoIzqdoB(n) != Abin<componente>::NODO_NULO){
+      if(A.elemento(n).operador == '+') return aritmeticaRec(A, A.hijoIzqdoB(n)) + aritmeticaRec(A, A.hijoDrchoB(n));
+      if(A.elemento(n).operador == '-') return aritmeticaRec(A, A.hijoIzqdoB(n)) - aritmeticaRec(A, A.hijoDrchoB(n));
+      if(A.elemento(n).operador == '*') return aritmeticaRec(A, A.hijoIzqdoB(n)) * aritmeticaRec(A, A.hijoDrchoB(n));
+      if(A.elemento(n).operador == '/') return aritmeticaRec(A, A.hijoIzqdoB(n)) / aritmeticaRec(A, A.hijoDrchoB(n));
+  }
+  return A.elemento(n).operando;
+}
+
+double aritmetica(Abin<componente>& A){
+  return aritmeticaRec(A, A.raizB());
+}
+
+int main(){
+  Abin<componente> a;
+  componente c;
+  c.operador = '+';
+  a.insertarRaizB(c);
+  c.operando = 1;
+  c.operador = '*';
+  a.insertarHijoIzqdoB(a.raizB(), c);
+  a.insertarHijoDrchoB(a.raizB(), c);
+  a.insertarHijoIzqdoB(a.hijoIzqdoB(a.raizB()), c);
+  a.insertarHijoDrchoB(a.hijoIzqdoB(a.raizB()), c);
+  std::cout << aritmetica(a) << std::endl;
+  //imprimirAbin(reflejado(a));
+}
