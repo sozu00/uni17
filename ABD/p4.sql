ab437d388f71d68664b8659b59ba77b2fb9d69a6
sozu00 <inhiguez.jesus@gmail.com>
2017-04-18 08:57:13 +0200

    update

--- ABD/p4.sql
+++ ABD/p4.sql
@@ -1,55 +1,55 @@
 use banco;
 
-CREATE TABLE Banco (
-  Numero_cuenta char(20) NOT NULL,
-  Nombre varchar(50) ,
-  Apellidos varchar(50) ,
-  Saldo float NOT NULL DEFAULT '0',
-  PRIMARY KEY (Numero_cuenta)
+CREATE TABLE cuenta (
+  nCuenta char(20) NOT NULL,
+  nombre varchar(20) ,
+  apellidos varchar(20) ,
+  saldo double NOT NULL DEFAULT 0,
+  PRIMARY KEY (nCuenta)
 );
 
-CREATE TABLE Operacion(
-  id INT AUTO_INCREMENT,
+CREATE TABLE operacion(
+  idOperacion INT AUTO_INCREMENT,
+  cuenta_nCuenta char(20) NOT NULL,
   asunto VARCHAR(100),
   fecha DATETIME NOT NULL DEFAULT NOW(),
-  cantidad FLOAT NOT NULL,
-  cuenta char(20) NOT NULL,
-  PRIMARY KEY(id),
-  CONSTRAINT cuenta FOREIGN KEY (cuenta) REFERENCES Banco(Numero_cuenta)
+  cantidad DOUBLE NOT NULL,
+  PRIMARY KEY(idOperacion),
+  CONSTRAINT cuenta FOREIGN KEY (cuenta_nCuenta) REFERENCES cuenta(nCuenta)
 );
 
 
 CREATE TRIGGER nueva_operacion
-    AFTER INSERT ON Operacion FOR EACH ROW
-    UPDATE Banco SET Saldo = Saldo + NEW.cantidad
-    where Numero_cuenta = NEW.cuenta;
+    AFTER INSERT ON operacion FOR EACH ROW
+    UPDATE cuenta SET saldo = saldo + NEW.cantidad
+    where nCuenta = NEW.cuenta_nCuenta;
 
 DELIMITER $$
 CREATE TRIGGER nueva_cuenta
-  BEFORE INSERT ON Banco FOR EACH ROW
+  BEFORE INSERT ON cuenta FOR EACH ROW
   BEGIN
-    IF NEW.Saldo < 0 THEN
-      SET NEW.Saldo = 0;
+    IF NEW.saldo < 0 THEN
+      SET NEW.saldo = 0;
     END IF;
   END$$
 DELIMITER ;
+/*
+INSERT INTO cuenta (nCuenta, Nombre, apellidos) VALUES ("12345678901234567890", "juan", "muñoz");
+INSERT INTO cuenta (nCuenta, Nombre, apellidos, saldo) VALUES ("12345678901234567891", "pepe", "perez", -200);
 
-INSERT INTO Banco (Numero_cuenta, Nombre, Apellidos) VALUES ("12345678901234567890", "juan", "muñoz");
-INSERT INTO Banco (Numero_cuenta, Nombre, Apellidos, Saldo) VALUES ("12345678901234567891", "pepe", "perez", -200);
+INSERT INTO operacion(asunto, cantidad, cuenta) VALUES ("incremento", 100, "12345678901234567890");
 
-INSERT INTO Operacion(asunto, cantidad, cuenta) VALUES ("incremento", 100, "12345678901234567890");
-
-
+*/
 DELIMITER $$
-CREATE PROCEDURE transaccion(IN cuenta1 CHAR(20), IN cuenta2 CHAR(20), IN cant FLOAT, IN concepto VARCHAR(100))
+CREATE PROCEDURE transaccion(IN cuenta1 CHAR(20), IN cuenta2 CHAR(20), IN cant DOUBLE, IN concepto VARCHAR(100))
 BEGIN
   DECLARE EXIT HANDLER FOR 1452 ROLLBACK;
   START TRANSACTION;
   IF cant < 0 THEN
     SIGNAL SQLSTATE 'ERROR' SET MESSAGE_TEXT = 'Cantidad negativa';
   END IF;
-  INSERT INTO Operacion(asunto, cantidad, cuenta) VALUES (concepto, -cant, cuenta1);
-  INSERT INTO Operacion(asunto, cantidad, cuenta) VALUES (concepto, cant, cuenta2);
+  INSERT INTO operacion(asunto, cantidad, cuenta_nCuenta) VALUES (concepto, -cant, cuenta1);
+  INSERT INTO operacion(asunto, cantidad,cuenta_nCuenta) VALUES (concepto, cant, cuenta2);
 
   COMMIT;
 END$$
